0d3ab1b84efad25a2097e8f597877058a23cb058
inguuu <dlsrb8949@gmail.com>
2019-08-09 19:06:44 +0900

    create repo

---
+++ src/main/java/com/example/mongotest/domain/Content.java
@@ -0,0 +1,8 @@
+package com.example.mongotest.domain;
+
+import lombok.Data;
+
+@Data
+
+public class Content {
+}
--- src/main/java/com/example/mongotest/model/Board.java
+++ src/main/java/com/example/mongotest/model/Board.java
@@ -1,12 +1,16 @@
 package com.example.mongotest.model;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
+@Document(collection = "deliver")
 public class Board {
-    private int idx;
-    private String title;
-    private String content;
+
+    @Id
+    private  int contentIdx;
+    private  String title;
 
 }
